c7cfd6beafe4c3f4578cadd6fa47c72900051aa1
gilagajet <84836081+gilagajet@users.noreply.github.com>
2022-09-13 12:43:20 +0800

    Update diy-part2.1.sh

--- devices/Totolink-X5000R/diy-part2.1.sh
+++ devices/Totolink-X5000R/diy-part2.1.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/OpenWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OpenWRT/g' package/base-files/files/bin/config_generate
 
 # Enable WiFi Interface
 sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
