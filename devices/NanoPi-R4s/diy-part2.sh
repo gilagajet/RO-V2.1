ca201cd91da3ce8bdea451604e15c8539c3dfe29
gilagajet <84836081+gilagajet@users.noreply.github.com>
2022-09-15 19:51:19 +0800

    Update diy-part2.sh

--- devices/NanoPi-R4s/diy-part2.sh
+++ devices/NanoPi-R4s/diy-part2.sh
@@ -35,6 +35,6 @@ sed -i 's/3.openwrt.pool.ntp.org/my.pool.ntp.org/g' package/base-files/files/bin
 
 
 ###Script
-wget https://raw.githubusercontent.com/gilagajet/gen/rockchip/import_feeds.sh
-chmod +x import_feeds.sh
-./import_feeds.sh
+#wget https://raw.githubusercontent.com/gilagajet/gen/rockchip/import_feeds.sh
+#chmod +x import_feeds.sh
+#./import_feeds.sh
